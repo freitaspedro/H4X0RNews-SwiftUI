735d7fefc5b78036621cff0ae75e05a82bb0ab80
pedro <pfreitas@id.uff.br>
2020-12-15 14:26:26 -0300

    Initial Commit

---
+++ HAXOR News SwiftUI/HAXOR_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAXOR_News_SwiftUIApp.swift
+//  HAXOR News SwiftUI
+//
+//  Created by Pedro Freitas | Zoop on 15/12/20.
+//
+
+import SwiftUI
+
+@main
+struct HAXOR_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
